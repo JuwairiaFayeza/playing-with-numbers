f2bb640cb2d1568248461ba901a97d3b6271c0a7
JuwairiaFayeza <nishatpoushi07@gmail.com>
2024-09-25 21:50:10 +0600

    playing with numbers

---
+++ main.py
@@ -0,0 +1,31 @@
+# num = int (input("enter number"))
+# originalnumber= num
+# reversenumber= 0
+# while num> 0:
+#     digit=num%10
+#     reversenumber=reversenumber*10+digit
+#     num //= 10
+# if originalnumber==reversenumber:
+#     print(originalnumber,"it is a pallindrone number")
+# else:
+#     print(originalnumber,"it is not a pallindrone number")
+
+# num = int (input("enter number"))
+# if num>0 :
+#     print(num,"It is a positive number")
+# else:
+#     print(num,"it is a negative number")
+
+# numberlargest= int (input("enter number"))
+# numbersmallest=int (input("enter number"))
+# while numbersmallest :
+#     numberstore= numbersmallest
+#     numbersmallest= numberlargest%numbersmallest
+#     numberlargest= numberstore
+# print("NCF is" , numberlargest)
+
+num = 10
+if num%2==0:
+    print("it is even number")
+else:
+    print("it is odd number")
